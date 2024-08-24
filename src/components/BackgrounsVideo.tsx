f38c017172eace8b17edf46d303482efb7e18fd2
SHAURYA PRATAP SINGH <shaurya.pratapsingh@airtel.com>
2024-08-24 22:18:13 +0530

    alignment fix

--- src/components/BackgrounsVideo.tsx
+++ src/components/BackgrounsVideo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const BackgroundVideo: React.FC = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen overflow-hidden">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
         autoPlay
@@ -14,16 +14,16 @@ const BackgroundVideo: React.FC = () => {
         <source src="/path-to-your-video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="relative md:px-20 lg:px-28 flex items-center justify-start py-12 px-4 w-full h-full bg-gradient-to-r from-black/40 to-transparent">
-        <div className="md:w-11/12 space-y-6">
-          <h1 className="text-black text-4xl md:text-5xl text-left font-extrabold drop-shadow-lg leading-snug">
+      <div className="relative flex flex-col  items-center justify-center py-12 px-4 w-full h-full bg-gradient-to-r from-black/40 to-transparent">
+        <div className="text-left md:text-left md:w-11/12 space-y-6">
+          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-extrabold drop-shadow-lg leading-snug">
             Transform Your Business And Growth With 360° Cutting-Edge Tech
             Solutions
           </h1>
-          <p className="text-xl text-black text-left font-normal font-OpenSans animate__pulse">
+          <p className="text-lg sm:text-xl md:text-2xl text-white font-normal font-OpenSans">
             Building Innovative Products And Making Them Successful
           </p>
-          <div className="py-4 space-x-4">
+          <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0">
             <Link to="/services">
               <button
                 className="bg-white text-[#55308D] font-OpenSans font-bold py-3 px-8 text-lg rounded-full shadow-lg hover:bg-[#f0f0f0] transition-transform transform hover:scale-105"
