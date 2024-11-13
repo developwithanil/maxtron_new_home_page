af6f600fc4df5b9573cf2f1a6ea7f98e442a5e5f
Riddhi10963 <riddhijais92@gmail.com>
2024-11-13 18:55:20 +0530

    removed comments

--- src/components/elementalComponent/Card.tsx
+++ src/components/elementalComponent/Card.tsx
@@ -4,26 +4,21 @@ interface CardProps {
   title: string;
   subtitle: string;
   description: string;
-  // icon: React.ReactNode;
+
   imageUrl: string;
 }
 
 const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
   return (
-    <div
-      // style={{ backgroundImage: "url(/cardbg.svg)" }}
-      className=" text-[#1E1E1E] bg-cover bg-[#FFEFFC] bg-center p-4 rounded-xl shadow-lg transform transition-transform duration-300 hover:scale-110 hover:shadow-xl"
-    >
+    <div className=" text-[#1E1E1E] bg-cover bg-[#FFEFFC] bg-center p-4 rounded-xl shadow-lg transform transition-transform duration-300 hover:scale-110 hover:shadow-xl">
       <div className="flex flex-col items-start pl-2 pt-1">
         <div className="text-xl mb-2 ">
-          <img
-            src={imageUrl}
-            alt={title}
-            className="w-7 h-7 rounded-full "
-          />
+          <img src={imageUrl} alt={title} className="w-7 h-7 rounded-full " />
         </div>
 
-        <h3 className="text-xl text-[#4E009C] font-OpenSans text-left font-bold">{title}</h3>
+        <h3 className="text-xl text-[#4E009C] font-OpenSans text-left font-bold">
+          {title}
+        </h3>
 
         <p className="bg-[#FFFFFF]  bg-opacity-90 text-[#4E009C] mt-4 sm:min-h-[150px]  mb-1 p-3 flex text-left justify-stretch font-normal text-sm rounded-xl font-OpenSans">
           {description}
