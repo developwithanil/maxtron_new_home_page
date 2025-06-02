c5ef7d240f10ff82f0016c4fa891cd5df25c7d4c
developwithanil <anil@maxtron.ai>
2025-06-02 19:12:55 +0530

    fix

--- src/components/pages/FundRaising.tsx
+++ src/components/pages/FundRaising.tsx
@@ -1,11 +1,57 @@
 import ContactAndSubscribe from "../elementalComponent/ContactAndSubscribe";
 import fund from "../../assets/case/fund.svg";
+import whatwedo1 from "../../assets/case/whatwedo1.svg";
+import whatwedo2 from "../../assets/case/whatwedo2.svg";
+import whatwedo3 from "../../assets/case/whatwedo3.svg";
+import whatwedo4 from "../../assets/case/whatwedo4.svg";
+import whatwedo5 from "../../assets/case/whatwedo5.svg";
+import whatwedo6 from "../../assets/case/whatwedo6.svg";
+import whatwedo7 from "../../assets/case/whatwedo7.svg";
+import whatwedo8 from "../../assets/case/whatwedo8.svg";
+import whatwedo9 from "../../assets/case/whatwedo9.svg";
+import whatwedo10 from "../../assets/case/whatwedo10.svg";
+
 import { Link } from "react-router-dom";
+
+const items = [
+    {
+        icon:whatwedo1,
+        title:"Token Fundraising Strategy & Execution"
+    },
+    {
+        icon:whatwedo2,
+        title:"NBFC & Line of Credit Support"
+    },
+    {
+        icon:whatwedo3,
+        title:"Access to Global VC & Angel Networks"
+    },
+    {
+        icon:whatwedo4,
+        title:"Smart Legal Structuring for Web3"
+    },
+    {
+        icon:whatwedo5,
+        title:"Valuation Guidance & Strategic Positioning"
+    },
+    {
+        icon:whatwedo6,
+        title:"Tokenomics Design"
+    },
+    {
+        icon:whatwedo7,
+        title:"Launch Support & Early Believer Campaigns"
+    },
+    {
+        icon:whatwedo8,
+        title:"Liquidity Planning & Staking Models"
+    },
+]
 
 const FundRaising = () => {
   return (
     <div className="w-full">
-      <div className="w-full max-w-[96rem] mx-auto px-4 sm:px-6 md:px-8 xl:px-6 py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12">
+      <div className="w-full max-w-[96rem] mx-auto px-4 sm:px-6 md:px-8 xl:px-6 py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 ">
         <div className="flex flex-col lg:flex-row gap-5 sm:gap-7 md:gap-9 lg:gap-10">
           {/* Text Content */}
           <div className="flex-1 flex flex-col justify-center items-start px-2 sm:px-3 md:px-4 lg:px-5 xl:px-7 gap-4 md:gap-5 lg:gap-6 xl:gap-8">
@@ -45,7 +91,48 @@ const FundRaising = () => {
             />
           </div>
         </div>
+        <div className="w-full  mt-12 md:mt-16 lg:mt-20 xl:mt-24">
+            <h2 className="text-[#2A2A2A] font-[Switzer] text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold text-center mt-8 md:mt-10 lg:mt-12 xl:mt-16">
+                What We Do
+            </h2>
+            <p className="text-center mt-3 lg:mt-4 xl:mt-5 self-stretch text-[#79748A] font-openSansHebrew  text-base md:text-lg lg:text-xl font-normal leading-[160%]">
+Everything You Need to Go from Vision to Valuation
+            </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 mt-8 md:mt-10 lg:mt-12 xl:mt-16">
+            {items.map((item, index) => (
+                <div
+                key={index}
+                className="flex flex-col items-start gap-5 border border-[#DFDEE7] p-4 rounded-3xl border-solid"
+                >
+                    
+                <img
+                    src={item.icon}
+                    alt={item.title}
+                    className="w-16 h-16 p-2 bg-[#F5F5F5] rounded-lg"
+                />
+                <h3 className="self-stretch text-[#2A2A2A] font-[Switzer] text-xl lg:text-2xl font-bold leading-[140%]">
+                    {item.title}
+                </h3>
+                </div>
+            ))}
+            <div
+  className="flex items-center justify-center gap-5 border border-[#DFDEE7] rounded-3xl border-solid bg-center bg-no-repeat text-center p-6"
+  style={{
+    background:
+      "radial-gradient(187.13% 114.42% at 50% 50%, #B58AE5 0%, #7A35C1 100%)",
+  }}
+>
+  <img src={whatwedo9} alt="left" className="h-24" />
+  <h3 className="text-white font-[Switzer] text-2xl font-bold leading-[140%]">
+    Trusted by pioneering founders building in 10+ countries.
+  </h3>
+  <img src={whatwedo10} alt="right" className="h-24" />
+</div>
+
+        </div>
+        </div>
       </div>
+     
 
       <ContactAndSubscribe />
     </div>
