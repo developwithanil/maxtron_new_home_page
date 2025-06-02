afdd6abc1020e1142b935a5b1903f0c797b1d500
developwithanil <anil@maxtron.ai>
2025-06-02 15:05:57 +0530

    fix width

--- src/components/elementalComponent/ContactAndSubscribe.tsx
+++ src/components/elementalComponent/ContactAndSubscribe.tsx
@@ -4,7 +4,7 @@ import { WhiteLogo } from "../VectorImage";
 const ContactAndSubscribe = () => {
   return (
     <div className="w-full relative -bottom-4 sm:-bottom-6 md:-bottom-8 lg:-bottom-10 xl:-bottom-12 bg-[#1D1B33] pb-10">
-      <div className="w-full max-w-screen-xl mx-auto px-4 sm:px-6 md:px-12 flex flex-col md:flex-row items-center justify-between gap-10 py-10">
+      <div className="w-full max-w-[96rem] mx-auto px-4 sm:px-6 md:px-12 flex flex-col md:flex-row items-center justify-between gap-10 py-10">
         {/* Logo */}
         <div className="w-full md:w-1/2 flex justify-center md:justify-start">
           <WhiteLogo />
