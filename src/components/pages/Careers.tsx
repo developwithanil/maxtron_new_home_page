f454b767e622a06654ed926b509faa30ecad1e9b
developwithanil <anil@maxtron.ai>
2025-06-26 11:16:30 +0530

    fix

--- src/components/pages/Careers.tsx
+++ src/components/pages/Careers.tsx
@@ -26,11 +26,11 @@ const Careers = () => {
     <div className="w-full">
       <div className="w-full max-w-[96rem] mx-auto flex flex-col gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-12 mt-5 md:mt-6 lg:mt-8 xl:mt-10 px-5 md:px-8 xl:px-6">
         <div className="flex w-full flex-col-reverse lg:flex-row gap-[14px] md:gap-[32px] lg:gap-[44px]  ">
-          <div className="flex-1">
+          <div className="flex-1 ">
             <img
               src={career}
               alt={"Career Image"}
-              className="w-full h-full object-cover rounded-xl px-3 md:px-4 lg:px-7 lg:py-4"
+              className="w-full h-[470px] lg:h-[550px] object-contain rounded-xl px-3 md:px-4 lg:px-7 lg:py-4"
             />
           </div>
           <div className="flex-1 lg:py-6 lg:pt-12">
