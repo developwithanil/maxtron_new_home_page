afecac1e78e391fe2b491d587e70f15a14d78f73
anil kumar <anil@maxtron.ai>
2025-07-03 15:33:13 +0530

    fix

--- src/components/pages/OurTeam.tsx
+++ src/components/pages/OurTeam.tsx
@@ -11,21 +11,21 @@ const OurTeam = () => {
   return (
     <div className="w-full">
       <div className="w-full max-w-[96rem] mx-auto pl-4 sm:pl-7 md:pl-12 xl:pl-14 py-4 sm:py-6 md:py-8 lg:py-10 xl:py-12 ">
-        <div className="flex flex-col lg:flex-row justify-between">
+        <div className="flex flex-row gap-7">
   
-          <div className="w-full lg:w-[32%] hidden lg:flex flex-col justify-center items-start ">
+          <div className="w-[32%] hidden lg:flex flex-col justify-center items-start">
             <img
               src={ourteamrightimage}
               alt="Affiliate team"
-              className="w-full h-auto lg:h-[25rem] xl:h-[30rem] 2xl:h-[34rem] object-fit"
+              className="w-full h-[25rem] lg:h-[27rem] xl:h-[34rem] object-fit"
             />
           </div>
 
-          <div className="w-full lg:w-[64%] xl:w-[62%] 2xl:w-[64%]  flex items-center">
+          <div className="flex-1 flex items-end pr-0">
             <img
               src={affiliaterightlanding}
               alt="Affiliate team"
-              className="w-full h-auto   2xl:h-[34rem] object-contain filter grayscale"
+              className="w-full h-[25rem] lg:h-[27rem] xl:h-[34rem] object-fit filter grayscale"
             />
           </div>
         </div>
