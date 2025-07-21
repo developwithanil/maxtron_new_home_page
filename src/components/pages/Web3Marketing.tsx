f031cdd1243120b9fb66fe1e616afb310c7c70b4
mehakmehak221 <113156603+mehak-2@users.noreply.github.com>
2025-07-21 14:19:31 +0530

    meta tags for web3 marketing page

--- src/components/pages/Web3Marketing.tsx
+++ src/components/pages/Web3Marketing.tsx
@@ -10,10 +10,12 @@ const Web3Marketing = () => {
   return (
     <>
       <Helmet>
-        <title>Web3 Marketing | Maxtron</title>
+        <title>
+          Best Web3 Marketing & PR Agency | Top Web3 digital marketing agency
+        </title>
         <meta
           name="description"
-          content="Maxtron's Web3 Marketing services accelerate your business growth with innovative blockchain and digital marketing solutions."
+          content="Looking for the best Web3 marketing agency? We are a leading Web3 marketing company offering PR, advertising, and digital marketing solutions for blockchain brands. Partner with top Web3 marketing experts today."
         />
         <link rel="canonical" href="https://www.maxtron.ai/web3-marketing" />
         <meta name="robots" content="index, follow" />
