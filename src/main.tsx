8a37cb844c378c18d2a1c9ea07b73d704c041240
mehakmehak221 <113156603+mehak-2@users.noreply.github.com>
2025-08-20 17:03:17 +0530

    current event removed

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
 // import { Helmet } from "react-helmet";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
